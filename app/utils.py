6403dc7492f27b4e15f9db6c6ce2c6ca6b51b4fe
Snuffy2 <Snuffy2@users.noreply.github.com>
2023-11-26 17:54:46 -0500

    Make parenthesis and the text in it and optional and zero to multiple spaces

--- app/utils.py
+++ app/utils.py
@@ -24,8 +24,15 @@ def upperescape(string):
     string = string.replace("’", "'")
     string = string.replace("“", '"')
     string = string.replace("”", '"')
+    # Replace >1 space with single space
+    string = re.sub(" +", " ", string)
     # Escape the characters
     string = re.escape(string)
+    # Handle none to multiple spaces
+    string = string.replace("\\ ", "[\\ ]*")
+    # Make parenthesis optional
+    string = string.replace("\\(", "([\\(]?")
+    string = string.replace("\\)", "[\\)]?)?")
     # Make it look for and as whole or ampersands
     string = string.replace("\\ AND\\ ", "\\ (AND|&)\\ ")
     # Make punctuation optional for human error
@@ -139,7 +146,7 @@ def ytdl_hooks(d):
 
 def setup_logging(lf_enabled=True, lc_enabled=True, debugging=False):
     log_level = logging.INFO
-    log_level = logging.DEBUG if debugging == True else log_level
+    log_level = logging.DEBUG if debugging is True else log_level
     logger = logging.getLogger("sonarr_youtubedl")
     logger.setLevel(log_level)
     log_format = logging.Formatter(
